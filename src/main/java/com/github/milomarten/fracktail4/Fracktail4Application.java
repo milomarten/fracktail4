9f010070c201c66f26409b64aacd3f62c56183dd
Milo Marten <milomartenstreams@gmail.com>
2023-09-19 20:59:26 -0500

    initial commit

---
+++ src/main/java/com/github/milomarten/fracktail4/Fracktail4Application.java
@@ -0,0 +1,13 @@
+package com.github.milomarten.fracktail4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fracktail4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fracktail4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/milomarten/fracktail4/Fracktail4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.milomarten.fracktail4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fracktail4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
