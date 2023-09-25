26f5108dd22c89a1b284a6674670a61ba8f310a5
Milo Marten <milomartenstreams@gmail.com>
2023-09-24 23:33:11 -0500

    Migrate persistence to S3

---
+++ src/main/java/com/github/milomarten/fracktail4/AWSBootstrap.java
@@ -0,0 +1,27 @@
+package com.github.milomarten.fracktail4;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AWSBootstrap {
+    @Bean
+    public AmazonS3 amazonS3(
+            @Value("${aws.access-key}") String accessKey,
+            @Value("${aws.token}") String secret,
+            @Value("${aws.region}") Regions regions) {
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(
+                        accessKey,
+                        secret
+                )))
+                .withRegion(regions)
+                .build();
+    }
+}
---
+++ src/main/java/com/github/milomarten/fracktail4/persistence/AWSPersistence.java
@@ -0,0 +1,54 @@
+package com.github.milomarten.fracktail4.persistence;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.io.File;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AWSPersistence implements Persistence {
+    private static final String BUCKET_NAME = "fracktail-persistence";
+
+    private final AmazonS3 s3;
+    private final ObjectMapper mapper;
+
+    @Override
+    public Mono<Void> store(String key, Object value) {
+        return Mono.fromCallable(() -> {
+            String contents = mapper.writeValueAsString(value);
+            s3.putObject(BUCKET_NAME, key, contents);
+            return null;
+        });
+    }
+
+    @Override
+    public <T> Mono<T> retrieve(String key, Class<T> clazz) {
+        return Mono.fromCallable(() -> {
+            var object = s3.getObject(BUCKET_NAME, key);
+            return mapper.readValue(object.getObjectContent(), clazz);
+        });
+    }
+
+    @Override
+    public <T> Mono<T> retrieve(String key, TypeReference<T> clazz) {
+        return Mono.fromCallable(() -> {
+            var object = s3.getObject(BUCKET_NAME, key);
+            return mapper.readValue(object.getObjectContent(), clazz);
+        });
+    }
+
+    @Override
+    public Mono<Boolean> hasKey(String key) {
+        return Mono.just(false);
+    }
+}
