dc783a71741aa8fbb59c851a0bf2ad71d522921c
milomarten <milomartenstreams@gmail.com>
2024-11-02 21:55:54 -0500

    Deploy code was...easy?

--- src/main/java/com/github/milomarten/fracktail4/platform/discord/DiscordBootstrap.java
+++ src/main/java/com/github/milomarten/fracktail4/platform/discord/DiscordBootstrap.java
@@ -1,11 +1,19 @@
 package com.github.milomarten.fracktail4.platform.discord;
 
+import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.object.entity.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 @ConditionalOnProperty(prefix = "discord", name = "token")
@@ -18,5 +26,16 @@ public class DiscordBootstrap {
     @Bean
     public GatewayDiscordClient gatewayDiscordClient(DiscordClient discordClient) {
         return discordClient.login().block();
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> onReadyDiscord(GatewayDiscordClient client) {
+        return event -> client.getUserById(Snowflake.of(248612704019808258L))
+                .flatMap(User::getPrivateChannel)
+                .flatMap(pc -> {
+                    LocalDateTime ldt = LocalDateTime.now();
+                    return pc.createMessage("Good morning! It is " + DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(ldt) + ", and I am ready to serve.");
+                })
+                .subscribe((obj) -> {}, (err) -> {}, () -> {});
     }
 }
