2c99e076fd9ca84be03531f02ed66fa0e6e6fe0e
milomarten <milomartenstreams@gmail.com>
2024-11-03 00:42:36 -0500

    Secondary logic for reminding

--- src/main/java/com/github/milomarten/fracktail4/remind/RemindConfig.java
+++ src/main/java/com/github/milomarten/fracktail4/remind/RemindConfig.java
@@ -3,18 +3,21 @@ package com.github.milomarten.fracktail4.remind;
 import com.github.milomarten.fracktail4.persistence.Persistence;
 import com.github.milomarten.fracktail4.persistence.PersistingObject;
 import discord4j.common.util.Snowflake;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 @Configuration
 public class RemindConfig {
-    public PersistingObject<Map<Snowflake, ReminderJob>> reminderMap(Persistence persistence) {
+    @Bean
+    public PersistingObject<Map<Snowflake, List<ReminderJob>>> reminderMap(Persistence persistence) {
         return new PersistingObject<>(persistence, "remind-me") {
             @Override
-            protected Map<Snowflake, ReminderJob> createInitial() {
+            protected Map<Snowflake, List<ReminderJob>> createInitial() {
                 return new HashMap<>();
             }
         };
--- src/main/java/com/github/milomarten/fracktail4/remind/ReminderJob.java
+++ src/main/java/com/github/milomarten/fracktail4/remind/ReminderJob.java
@@ -1,8 +1,20 @@
 package com.github.milomarten.fracktail4.remind;
 
 import discord4j.common.util.Snowflake;
+import org.springframework.scheduling.Trigger;
+import org.springframework.scheduling.TriggerContext;
 
 import java.time.Instant;
 
-public record ReminderJob(Instant when, Snowflake where, String content, boolean ephemeral) {
+public record ReminderJob(Instant when, Snowflake where, String content, boolean ephemeral) implements Trigger {
+    @Override
+    public Instant nextExecution(TriggerContext triggerContext) {
+        // These jobs should happen once and only once. Thus, they should return null
+        // as soon as the job has run once.
+        if (triggerContext.lastCompletion() == null) {
+            return this.when;
+        } else {
+            return null;
+        }
+    }
 }
