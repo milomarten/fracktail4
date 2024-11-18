5f16599a2e34f8de3c0e316a540dcc6296863536
milomarten <milomartenstreams@gmail.com>
2024-11-17 21:01:44 -0600

    Templating is successful!

--- src/main/java/com/github/milomarten/fracktail4/config/TemplatingConfig.java
+++ src/main/java/com/github/milomarten/fracktail4/config/TemplatingConfig.java
@@ -36,9 +36,6 @@ public class TemplatingConfig implements ApplicationContextAware {
         engine.setEnableSpringELCompiler(true);
         engine.addDialect(new Java8TimeDialect());
 
-        var out = engine.process("sample", new Context(Locale.US, Map.of("planet", "mars")));
-        System.out.println("HEY! --->" + out);
-
         return engine;
     }
 
