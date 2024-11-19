67298de5cfa2b057b48fa7904bb612a39e3697ca
milomarten <milomartenstreams@gmail.com>
2024-11-18 22:34:17 -0600

    fix - use timezones correctly for via and brightline

--- src/main/java/com/github/milomarten/fracktail4/amtrak/parameters/AmtrakTrainLookup.java
+++ src/main/java/com/github/milomarten/fracktail4/amtrak/parameters/AmtrakTrainLookup.java
@@ -46,9 +46,6 @@ public class AmtrakTrainLookup implements AmtrakLookup {
             train = root.getGateway().getTrains(trainnumber).next();
         }
 
-        root.getGateway().getStaleStatus()
-                .subscribe(System.out::println);
-
         return Responses.delayedReply(train
                 .map(this::toView)
                 .map(view -> root.templateResponse("train-lookup", view))
@@ -59,48 +56,6 @@ public class AmtrakTrainLookup implements AmtrakLookup {
                 })
                 .onErrorReturn("Sorry, I had trouble getting that train.")
         );
-
-//        return Responses.delayedReply(train
-//                        .map(t -> {
-//                    // Train <code> is the <heading> <train name>
-//                    // It left <station> at <time>, and arrived at its final destination <station> at <time>
-//                    // OR It left <station> at <time>, and will arrive at its final destination <station> at <time>
-//                    // It is currently in <station>, as of <update time>
-//                    var lineOne = String.format("%s Train %s is the %s, heading %s.", t.getProvider(), t.getTrainNum(), t.getRouteName(), t.getHeading());
-//                    var firstStation = CollectionUtils.firstElement(t.getStations());
-//                    var finalStation = CollectionUtils.lastElement(t.getStations());
-//                    if (firstStation == null || finalStation == null) {
-//                        return lineOne + "\n" + "The route is empty, I can't provide any more information.";
-//                    }
-//
-//                    String lineTwo;
-//                    if (finalStation.getStatus() == RouteStatus.ENROUTE || finalStation.getStatus() == RouteStatus.UNKNOWN) {
-//                        lineTwo = String.format("It left %s on %s, and is scheduled to arrive at its final destination of %s on %s.",
-//                                firstStation.getName(), FORMATTER.format(firstStation.getDeparture()),
-//                                finalStation.getName(), FORMATTER.format(finalStation.getScheduledArrival())
-//                        );
-//
-//                        var upcomingStation = t.getUpcomingStation();
-//                        String lineThree = String.format("It is currently approaching %s, as of %s. %s(all times are local time)",
-//                                t.getEventName(), FORMATTER.format(t.getUpdatedAt()
-//                                        .withZoneSameInstant(t.getEventTimezone().toZoneId())),
-//                                upcomingStation == null ? "" :
-//                                        "It's estimated to arrive there on " + FORMATTER.format(upcomingStation.getArrival()) + ". "
-//                        );
-//
-//                        return lineOne + "\n" + lineTwo + "\n" + lineThree;
-//                    } else {
-//                        lineTwo = String.format("It left %s on %s, and arrived at its final destination of %s on %s (all times are local time).",
-//                                firstStation.getName(), FORMATTER.format(firstStation.getDeparture()),
-//                                finalStation.getName(), FORMATTER.format(finalStation.getArrival())
-//                        );
-//
-//                        return lineOne + "\n" + lineTwo;
-//                    }
-//                })
-//                .switchIfEmpty(Mono.just("Sorry, I don't know that train."))
-//                .onErrorResume(e -> Mono.just("Sorry, I don't know that train."))
-//        );
     }
 
     private static final Set<TrainState> IN_MOTION_STATUSES =
@@ -161,6 +116,17 @@ public class AmtrakTrainLookup implements AmtrakLookup {
                                    ZonedDateTime estDeparture, ZonedDateTime estArrival) {
         public static DepartureArrival of(RouteStation rs) {
             if (rs == null) { return null; }
+            if (rs.getCode().length() == 4) {
+                // Non-Amtrak station times are in UTC, so we have to normalize with the timezone sadly.
+                // VIA and Brightline stations are all four-letters
+                var timezone = rs.getTimezone().toZoneId();
+                return new DepartureArrival(
+                        rs.getScheduledDeparture().withZoneSameInstant(timezone),
+                        rs.getScheduledArrival().withZoneSameInstant(timezone),
+                        rs.getDeparture().withZoneSameInstant(timezone),
+                        rs.getArrival().withZoneSameInstant(timezone)
+                );
+            }
             return new DepartureArrival(
                     rs.getScheduledDeparture(), rs.getScheduledArrival(),
                     rs.getDeparture(), rs.getArrival()
