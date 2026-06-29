ce6ea6299a67b4e9ce69248b40f314f598650620
Justis Lamanna <lamanna.justis@gmail.com>
2026-06-29 17:17:53 -0500

    Moon announcement now says the moon's name

--- src/main/java/com/github/milomarten/fracktail/core/job/MoonJob.java
+++ src/main/java/com/github/milomarten/fracktail/core/job/MoonJob.java
@@ -11,9 +11,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.time.ZoneId;
 import java.time.chrono.HijrahDate;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.util.EnumMap;
+import java.util.Map;
 
 @Configuration
 @Slf4j
@@ -26,14 +31,38 @@ public class MoonJob extends AbstractAnnouncementJob {
     public static final String HOME_TIMEZONE_RAW = "America/New_York";
     public static final ZoneId HOME_TIMEZONE = ZoneId.of(HOME_TIMEZONE_RAW);
 
+    private static final String[] MOON_NAMES = {
+            "", // Sentinel...no Month 0
+            "Wolf", "Snow", "Worm", "Pink", "Flower", "Strawberry",
+            "Buck", "Sturgeon", "Harvest", "Hunter's", "Beaver", "Cold"
+    };
+
     @Scheduled(cron = "@midnight", zone = HOME_TIMEZONE_RAW)
     public void announceMoon() {
         var nowInIslamic = HijrahDate.now(HOME_TIMEZONE);
+        var nowInUS = LocalDate.now(HOME_TIMEZONE);
 
         var daysInThisMonth = nowInIslamic.lengthOfMonth();
 
         if (nowInIslamic.get(ChronoField.DAY_OF_MONTH) == daysInThisMonth / 2) {
-            sendAnnouncement("\uD83C\uDF15 \uD83D\uDC3A");
+            if (isBlueMoon(nowInIslamic, nowInUS)) {
+                sendAnnouncement("\uD83D\uDD35", "Blue");
+            } else {
+                sendAnnouncement("\uD83C\uDF15", MOON_NAMES[nowInUS.getMonthValue()]);
+            }
         }
+    }
+
+    private boolean isBlueMoon(HijrahDate todayInHijrah, LocalDate todayInUS) {
+        var oneMonthAgo = todayInHijrah.minus(1, ChronoUnit.MONTHS);
+        var lastFullMoon = oneMonthAgo.with(ChronoField.DAY_OF_MONTH, oneMonthAgo.lengthOfMonth() / 2);
+
+        var lastFullMoonUS = LocalDate.from(lastFullMoon);
+
+        return todayInUS.getMonth() == lastFullMoonUS.getMonth();
+    }
+
+    private void sendAnnouncement(String moon, String label) {
+        sendAnnouncement(moon + " \uD83D\uDC3A\n-# Hello to the " + label + " Moon!");
     }
 }
