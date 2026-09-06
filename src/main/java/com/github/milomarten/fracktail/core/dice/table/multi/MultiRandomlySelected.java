944321c97788e101dfa2d8c702ca0bf718717cdc
Justis Lamanna <lamanna.justis@gmail.com>
2026-09-05 22:48:42 -0500

    Remove table command test

--- src/main/java/com/github/milomarten/fracktail/core/dice/table/multi/MultiRandomlySelected.java
+++
@@ -1,9 +0,0 @@
-package com.github.milomarten.fracktail.core.dice.table.multi;
-
-import com.github.milomarten.fracktail.core.dice.table.RandomlySelected;
-
-import java.util.List;
-
-public interface MultiRandomlySelected<T> extends RandomlySelected<T> {
-    List<T> getMultiple(MultiRollParams params);
-}
